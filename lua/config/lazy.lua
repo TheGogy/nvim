b845e8c906e9bcce1de950364046c95ec48b2832
Gogy <thegogy@proton.me>
2024-04-04 04:17:45 +0100

    updated colourscheme to fit with rest of theme properly

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,7 +10,7 @@ require("lazy").setup({
 	spec = {
 		-- add LazyVim and import its plugins
 		{ "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
-			colorscheme = "oxocarbon",
+			colorscheme = "horizon",
 		} },
 		{ import = "plugins" },
 	},
