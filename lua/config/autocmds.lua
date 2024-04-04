65dfe428932acc00c3fbc59918e3b259d74dee92
Gogy <thegogy@proton.me>
2024-04-04 04:30:04 +0100

    fixed telescope colours

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,21 +2,17 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.api.nvim_create_autocmd('ColorScheme', {
-  callback = function()
 
-    -- Set colours for Telescope
-    vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "#232530" })
-    vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "#232530" })
-    vim.api.nvim_set_hl(0, "TelescopeSelectionCaret", { bg = "#232530" })
-    vim.api.nvim_set_hl(0, "TelescopeMatching", { bg = "#232530" })
-    vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "#2E303E" })
-    vim.api.nvim_set_hl(0, "TelescopePromptTitle", { bg = "#E95678", fg = "#06060C" })
-    vim.api.nvim_set_hl(0, "TelescopePromptPrefix", { bg = "#2E303E" })
-    vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "#2E303E" })
-    vim.api.nvim_set_hl(0, "TelescopePreviewTitle", { bg = "#232530" })
-    vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { bg = "#232530" })
-    vim.api.nvim_set_hl(0, "TelescopeResultsTitle", { bg = "#232530" })
-    vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "#232530" })
-  end
-})
+-- Fix colours for telescope
+vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "#232530" })
+vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "#232530" })
+vim.api.nvim_set_hl(0, "TelescopeSelectionCaret", { bg = "#232530" })
+vim.api.nvim_set_hl(0, "TelescopeMatching", { bg = "#232530" })
+vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "#2E303E" })
+vim.api.nvim_set_hl(0, "TelescopePromptTitle", { bg = "#E95678", fg = "#06060C" })
+vim.api.nvim_set_hl(0, "TelescopePromptPrefix", { bg = "#2E303E" })
+vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "#2E303E" })
+vim.api.nvim_set_hl(0, "TelescopePreviewTitle", { bg = "#232530" })
+vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { bg = "#232530" })
+vim.api.nvim_set_hl(0, "TelescopeResultsTitle", { bg = "#232530" })
+vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "#232530" })
