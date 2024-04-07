b22ddb0915b74c85ecc603834961f9890c756fc8
Gogy <thegogy@proton.me>
2024-04-07 21:18:42 +0100

    themed bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,16 +3,28 @@ return {
     "akinsho/bufferline.nvim",
     event = "VeryLazy",
     config = function()
-      local bufferline = require("bufferline")
       require("bufferline").setup({
         options = {
-          style_preset = { bufferline.style_preset.minimal },
           left_trunc_marker = "",
           right_trunc_marker = "",
           always_show_bufferline = true,
           show_close_icon = false,
           show_buffer_close_icons = false,
         },
+        highlights = { -- Horizon theme
+          buffer_selected = {
+            bg = "#232530",
+            fg = "#FAB28E",
+            bold = true,
+          },
+          indicator_selected = {
+            bg = "#232530",
+            fg = "#E9436F"
+          },
+          fill = {
+            bg = "#1C1E26",
+          },
+        }
       })
     end,
   },
