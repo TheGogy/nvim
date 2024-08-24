64847e48f0100b0266884a61049e3ef132dd72e3
George Rawlinson <thegogy@proton.me>
2024-08-24 13:55:22 +0100

    changed to tokyodark

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,60 +1,61 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  -- bootstrap lazy.nvim
-  -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+    -- bootstrap lazy.nvim
+    -- stylua: ignore
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
-  spec = {
-    -- add LazyVim and import its plugins
-    {
-      "LazyVim/LazyVim",
-      import = "lazyvim.plugins",
-      opts = {
-        -- colorscheme = "gruvbox-material",
-      },
+    spec = {
+        -- add LazyVim and import its plugins
+        {
+            "LazyVim/LazyVim",
+            import = "lazyvim.plugins",
+            opts = {
+                colorscheme = "tokyodark",
+            },
+        },
+        { import = "lazyvim.plugins.extras.dap.core" },
+        { import = "plugins" },
     },
-    { import = "plugins" },
-  },
-  defaults = {
-    lazy = true, -- Lazy-load user installed plugins
-    version = false, -- always use the latest git commit
-  },
-  -- install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        "2html_plugin",
-        "tohtml",
-        "getscript",
-        "getscriptPlugin",
-        "gzip",
-        "logipat",
-        "netrw",
-        "netrwPlugin",
-        "netrwSettings",
-        "netrwFileHandlers",
-        "matchit",
-        "tar",
-        "tarPlugin",
-        "rrhelper",
-        "spellfile_plugin",
-        "vimball",
-        "vimballPlugin",
-        "zip",
-        "zipPlugin",
-        "tutor",
-        "rplugin",
-        "syntax",
-        "synmenu",
-        "optwin",
-        "compiler",
-        "bugreport",
-        "ftplugin",
-      },
+    defaults = {
+        lazy = true, -- Lazy-load user installed plugins
+        version = false, -- always use the latest git commit
     },
-  },
+    -- install = { colorscheme = { "tokyonight", "habamax" } },
+    checker = { enabled = true }, -- automatically check for plugin updates
+    performance = {
+        rtp = {
+            disabled_plugins = {
+                "2html_plugin",
+                "tohtml",
+                "getscript",
+                "getscriptPlugin",
+                "gzip",
+                "logipat",
+                "netrw",
+                "netrwPlugin",
+                "netrwSettings",
+                "netrwFileHandlers",
+                "matchit",
+                "tar",
+                "tarPlugin",
+                "rrhelper",
+                "spellfile_plugin",
+                "vimball",
+                "vimballPlugin",
+                "zip",
+                "zipPlugin",
+                "tutor",
+                "rplugin",
+                "syntax",
+                "synmenu",
+                "optwin",
+                "compiler",
+                "bugreport",
+                "ftplugin",
+            },
+        },
+    },
 })
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,9 @@
+
+return {
+    "akinsho/toggleterm.nvim",
+    config = true,
+    keys = {
+        { "<C-/>", "<cmd>ToggleTerm size=40 direction=horizontal<cr>", desc = "Open terminal" }
+    }
+
+}
