0221d555156b9979786134d385bcb8d74f61182c
George Rawlinson <TheGogy@proton.me>
2025-01-30 23:33:45 +0000

    added rust setup to lazyvim.json

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,30 +1,30 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  event = "VeryLazy",
-  keys = {
-    -- Change find files to <leader>ff
-    { "<leader>/", false },
-    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-    { "<leader>fw", "<cmd>Telescope live_grep<cr>", desc = "Find Words" },
-  },
-  config = function()
-    require("telescope").setup({
-      pickers = {
-        buffers = {
-          sort_mru = true,
-          previewer = true,
-          -- theme = "ivy",
-        },
-      },
-      defaults = {
-        prompt_prefix = "> ",
-        border = {},
-        -- borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-        -- Hacky border disabling
-        -- borderchars = { " ", " ", " ", " ", " ", " ", " ", " " },
-        color_devicons = true,
-        use_less = true,
-      },
-    })
-  end,
+    "nvim-telescope/telescope.nvim",
+    event = "VeryLazy",
+    keys = {
+        -- Change find files to <leader>ff
+        { "<leader>/", false },
+        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+        { "<leader>fw", "<cmd>Telescope live_grep<cr>", desc = "Find Words" },
+    },
+    config = function()
+        require("telescope").setup({
+            pickers = {
+                buffers = {
+                    sort_mru = true,
+                    previewer = true,
+                    -- theme = "ivy",
+                },
+            },
+            defaults = {
+                prompt_prefix = "> ",
+                border = {},
+                -- borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+                -- Hacky border disabling
+                -- borderchars = { " ", " ", " ", " ", " ", " ", " ", " " },
+                color_devicons = true,
+                use_less = true,
+            },
+        })
+    end,
 }
