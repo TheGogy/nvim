6be97096972d06c852c55f8cb585455aa73d3398
George Rawlinson <TheGogy@proton.me>
2025-03-18 14:42:14 +0000

    catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -10,11 +10,31 @@ return {
             vim.cmd("let g:gruvbox_material_diagnostic_virtual_text='colored'")
             vim.cmd("let g:gruvbox_material_enable_bold=1")
             vim.cmd("let g:gruvbox_material_enable_italic=1")
-            vim.cmd([[colorscheme gruvbox-material]]) -- Set color scheme
+            -- vim.cmd([[colorscheme gruvbox-material]]) -- Set color scheme
             -- changing bg and border colors
             vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
             vim.api.nvim_set_hl(0, "LspInfoBorder", { link = "Normal" })
             vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
         end,
+    },
+    { "projekt0n/github-nvim-theme", name = "github-theme" },
+    { "vague2k/vague.nvim" },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {
+            style = "night",
+            transparent = true,
+        },
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        opts = {
+            flavour = "mocha",
+            transparent_background = false,
+        },
     },
 }
